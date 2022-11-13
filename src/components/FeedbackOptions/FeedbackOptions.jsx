8e43e746be38e5f73c65aa36db889c52a9d66dfa
Igor Smorodsky <ingerard@gmail.com>
2022-11-13 20:36:51 +0200

    refactored FeedbackOptions to func comp

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Component } from 'react';
 
 // Check types of props
 import PropTypes from 'prop-types';
 import { Button, ButtonsGroup } from './FeedbackOptions.styled';
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <ButtonsGroup>
-        {options.map(btn => {
-          return <Button id={btn} key={btn} onClick={onLeaveFeedback}>{btn}</Button>;
-        })}
-      </ButtonsGroup>
-    );
-  }
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ButtonsGroup>
+      {options.map(btn => {
+        return (
+          <Button id={btn} key={btn} onClick={onLeaveFeedback}>
+            {btn}
+          </Button>
+        );
+      })}
+    </ButtonsGroup>
+  );
+};
 
 export { FeedbackOptions };
 
